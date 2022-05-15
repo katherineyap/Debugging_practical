9cabaf15e3fc44e079e7e36f51c01c04b07fcdcc
katherineyap <katherineyap25@gmail.com>
2022-05-15 14:46:49 +0100

    done

--- to_debug_4.py
+++ to_debug_4.py
@@ -5,17 +5,17 @@ def palindrome(number):
     
     # reverse the given number
     reverse_num = 0
-    while number > 0
+    while number > 0:
         reminder = number % 10
-        reverse_num = (reverse_num * 10) + remider
+        reverse_num = (reverse_num * 10) + reminder
         number = number // 10
 
     # check numbers
-    if original_num != reverse_num:
+    if original_num == reverse_num:
         print("Given number is a palindrome")
     else:
         print("Given number is not a palindrome")
 
-palindrome('11881118811')
+palindrome(11881118811)
 palindrome(125542628)
 palindrome(123789987321)
