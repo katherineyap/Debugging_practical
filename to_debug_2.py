17239def0277602a89b401784db3e4e9e01ef9f8
katherineyap <katherineyap25@gmail.com>
2022-05-15 14:34:57 +0100

    done

--- to_debug_2.py
+++ to_debug_2.py
@@ -1,10 +1,10 @@
 capitals = {
 "France": "Paris",
-“Spain”: Madrid,
-“Germany”: “Berlin,
-“Japan'': “Tokyo”,
-“Norway”: “Oslo”
+"Spain": "Madrid",
+"Germany" : "Berlin",
+"Japan": "Tokyo",
+"Norway": "Oslo"
 }
 
-for country, city in capitals.items()
-    print(f”The capital of {country) is {‘city’}.”
+for country, city in capitals.items():
+    print(f"The capital of",country,"is",city,".”")
